169d59440144a03d25bb7fe6819806829c8775b0
garretts8 <gar21085@byui.edu>
2022-04-28 01:35:45 -0400

    Replaced numbers with x

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,20 @@
+board = [1,2,3,4,5,6,7,8,9]
+for i in board:
+  print(i)
+  
 print()
-print("1|2|3")
-print("-+-+-")
-print("4|5|6")
-print("-+-+-")
-print("7|8|9")
+print(f"{board[0]}|{board[1]}|{board[2]}")
+print('-+-+-')
+print(f"{board[3]}|{board[4]}|{board[5]}")
+print('-+-+-')
+print(f"{board[6]}|{board[7]}|{board[8]}")
+print()
+
+board = [1,2,3,4,5,6,7,8,9]
+for i in board:
+  print(i)
+  
+choosex = int(input("x's turn to choose a square (1-9: ) "))
+board[choosex - 1] = "x"
+print(board)
+  
